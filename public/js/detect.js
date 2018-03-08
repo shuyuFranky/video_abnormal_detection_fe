b2d0b20b580efe174940a25971f09ddb06faf482
shuyuFranky <shuyu_@pku.edu.cn>
2018-03-09 00:43:07 +0800

    version 0.2

--- public/js/detect.js
+++ public/js/detect.js
@@ -9,18 +9,24 @@ $(function() {
             var data_sort = data.sort((a, b) => b.wscore - a.wscore);
             console.log(data_sort.length);
             for (var i = 0; i < data_sort.length; i++) {
-                var str = '<div class="col-md-3 mb-3">' + 
+                var str_s = '<div class="col-md-3 mb-3">' + 
                             '<div class="card h-100">' +
                               '<div class="card-body">' + 
                                 '<h2 class="card-title">Top ' + (i+1).toString() + '</h2>' + 
                                 '<img src="img/' + data_sort[i].imgname + '" class="' + 'res"></img>' +
                               '</div>' + 
-                              '<div class="card-footer">' + 
-                                '<a href="#" class="btn btn-primary">' + data_sort[i].vname + '</a>' +
-                              '</div>' +
+                              '<div class="card-footer">';
+                if (data_sort[i].category !== 'baokong') {
+                    str_m = '<a href="#" class="btn btn-primary">' + data_sort[i].vname + '</a>';
+                }
+                else {
+                    str_m = '<a href="#" class="btn btn-danger">' + data_sort[i].vname + '</a>';
+                }
+                var str_e = '</div>' +
                             '</div>' + 
                           '</div>' + 
                           '<!-- /.col-md-3 -->';
+                var str = str_s + str_m + str_e;
                 $('#res-box').append(str);
             }
             console.log("fuck");
